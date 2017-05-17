d6a7818fbf84517496e1d5019d8424612fe0c3e0
Sai Chandramouli <schand31@asu.edu>
2017-05-17 01:07:54 -0700

    added the views and the controllers for store

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,4 +1,6 @@
 class StoreController < ApplicationController
   def index
+    @products = Product.all.order(:title)
+    # can be shortened to @products = Product.order(:title) => results int he same SQL query
   end
 end
